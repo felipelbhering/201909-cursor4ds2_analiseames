d9dd5a2844c15f251587c97a640a475d36203797
felipelbhering <felipelbhering@gmail.com>
2020-09-07 22:34:50 -0300

    trocando rds pro correto e organizacao

--- R/main.R
+++ R/main.R
@@ -1,15 +1,15 @@
-#carregando bibliotecas
-library(devtools)
+#instalando pacote
+devtools::install_github("felipelbhering/cursor4ds2analiseames", 
+                         force= TRUE)
 
 #carregando dados
-df_ames = readr::read_rds("./data/ames.rds")
+df_ames = readr::read_rds("./data_raw/ames.rds")
 
-
-#instalando pacote
-install_github("felipelbhering/cursor4ds2analiseames", force= TRUE)
 
 #chamando funcoes
-cursor4ds2analiseames::sumarizar_venda_media (df_ames, nome_coluna="venda_condicao")
+cursor4ds2analiseames::sumarizar_venda_media(df_ames, 
+                                             nome_coluna="venda_condicao")
 cursor4ds2analiseames::pegar_num_nas(df_ames, "piscina_qualidade")
-cursor4ds2analiseames::pegar_num_nas(df_ames, "piscina_qualidade", prop = TRUE)
+cursor4ds2analiseames::pegar_num_nas(df_ames, "piscina_qualidade",
+                                     prop = TRUE)
 
